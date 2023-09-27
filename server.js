ec0949ddf0d2b23e7c34291e3e656200a71637ba
Weston Kostura <weston.kostura@gmail.com>
2023-09-27 16:12:01 -0700

    routes working and can be pulled up with insomnia

--- server.js
+++ server.js
@@ -1,22 +1,47 @@
 const express = require('express');
-// const routes = require('./routes');
+const routes = require('./routes/routes');
 const sequelize = require('./config/connection');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// turn on routes
+app.use(routes);
+
+//routes for public folder
+app.use(express.static('public'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// turn on routes
-// app.use(routes);
+// function for getting notes
+async function getNotes() {
+  const notes = await fs.readFileSync('./db/db.json', 'utf8');
+  return JSON.parse(notes);
+}
 
-// GET Route for homepage
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, './public/index.html'))
-// );
+function saveNotes(notes) {
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes));
+}
+
+//Post route for notes api
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuid.v4();
+  const notes = getNotes();
+  notes.push(newNote);
+  saveNotes(notes);
+  res.json(newNote);
+});
+
+// GET Route for notes api
+app.get('/api/notes', (req, res) => {
+  res.json(getNotes());
+});
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}!`));
 });
